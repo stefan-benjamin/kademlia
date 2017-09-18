c06327ce10435f0d2cb8d183ec456369ac28a85a
Brynleif Andreasen <brynleif@brynleif.com>
2017-09-18 13:37:34 +0200

    First implementation of getClosestNodes

--- HelloNode/bucketManager.js
+++ HelloNode/bucketManager.js
@@ -9,14 +9,13 @@ class BucketManager {
 
     constructor() {
         this.buckets = new Map();
-        this.DISTANCE_IS_ZERO = "ERR1001";
     }
 
-    _selectBucket(contactId) {
+    _selectBucketForInserting(contactId) {
        var distance = utils.getDistance(globals.nodeId, contactId);
 
        if (distance <= 0) {
-           return this.DISTANCE_IS_ZERO;
+           return constants.ERROR_SELECT_BUCKET_DISTANCE_IS_ZERO;
        }
 
         var x = 0;
@@ -30,14 +29,59 @@ class BucketManager {
         return x;
     }
 
-    _getKNodesFromNeighbourBuckets(bucketIndex) {
+    _selectBucketForReading(nodeIdOfCurrentNode) {
+        var distanceFromMeToTargetNode = utils.getDistance(nodeIdOfCurrentNode, globals.nodeId);
+        var bucketIndex = -1;
 
+        if (this.buckets.has(bucketIndex)) {
+            bucketIndex = Math.floor(Math.log2(distanceFromMeToTargetNode));
+        }
+        else {
+            var offset = 1;
+
+            while (offset <= constants.BUCKET_NEIGHBOUR_SEARCH_RANGE) {
+
+                var rightOfBucket = bucketIndex + offset;
+                var leftOfBucket = bucketIndex - offset;
+
+                if (this.buckets.has(rightOfBucket)) {
+                    bucketIndex = rightOfBucket;
+                    break;
+                }
+                else if (leftOfBucket > 0 && this.buckets.has(leftOfBucket)) {
+                    bucketIndex = leftOfBucket;
+                    break;
+                }
+
+                offset++;
+            }
+        }
+
+        return bucketIndex;
     }
 
-    receiveNode(nodeId, contact) {
-        var appropriateBucket = this._selectBucket(nodeId);
+    _getTargetNodeElseGetBucketWithClosestNodes(nodeId, bucketIndex) {
+        var bucketWithClosestNodes = this.buckets.get(bucketIndex);
 
-        if (appropriateBucket === this.DISTANCE_IS_ZERO) {
+        result = {};
+
+        if (bucketWithClosestNodes.has(nodeId)) {
+            result.returnType = constants.GET_CLOSEST_NODE_FOUND_THE_NODE;
+            result.content = bucketWithClosestNodes.get(nodeId);
+        }
+        else {
+            result.returnType = constants.GET_CLOSEST_NODE_FOUND_A_BUCKET;
+            result.content = bucketWithClosestNodes;
+        }
+
+        return result;
+    }
+
+
+    receiveNode(nodeId, contact) {
+        var appropriateBucket = this._selectBucketForInserting(nodeId);
+
+        if (appropriateBucket === constants.ERROR_SELECT_BUCKET_DISTANCE_IS_ZERO) {
             console.log("WARNING: Unable to add myself to a bucket.");
             return false;
         }
@@ -51,37 +95,43 @@ class BucketManager {
     }
 
     getClosestNodes(nodeId) {
-        var distanceFromMeToTargetNode = utils.getDistance(nodeId, globals.nodeId);
-        var bucketIndex = Math.floor(Math.log2(distanceFromMeToTargetNode));
+        //var distanceFromMeToTargetNode = utils.getDistance(nodeId, globals.nodeId);
+        //var bucketIndex = Math.floor(Math.log2(distanceFromMeToTargetNode));
 
-        var result = {};
+        //var result = { returnType: constants.GET_CLOSEST_NODE_FOUND_NOTHING, content: {} };
 
 
-        if (this.buckets.has(bucketIndex)) {
-            result.bucket = this.buckets.get(bucketIndex);
-        }
-        else {
-            //try looking to the left and right of the current bucket index and see if we find a bucket
-            var offset = 1;
+        //if (this.buckets.has(bucketIndex)) {
+        //    result = this._getTargetNodeElseGetBucketWithClosestNodes(nodeId, bucketIndex);
+        //}
+        //else {
+        //    //try looking to the left and right of the current bucket index and see if we find a bucket
+        //    var offset = 1;
 
-            while (offset <= constants.BUCKET_NEIGHBOUR_SEARCH_RANGE) {
+        //    while (offset <= constants.BUCKET_NEIGHBOUR_SEARCH_RANGE) {
                 
-                var rightOfBucket = bucketIndex + offset;
-                var leftOfBucket = bucketIndex - offset;
+        //        var rightOfBucket = bucketIndex + offset;
+        //        var leftOfBucket = bucketIndex - offset;
 
-                if (this.buckets.has(rightOfBucket)) {
-                    result.bucket = this.buckets.get(rightOfBucket);
-                    break;
-                }
-                else if (leftOfBucket > 0 && this.buckets.has(leftOfBucket)) {
-                    result.bucket = this.buckets.get(leftOfBucket);
-                    break;
-                }
+        //        if (this.buckets.has(rightOfBucket)) {
+        //            result.content = this.buckets.get(rightOfBucket);
+        //            break;
+        //        }
+        //        else if (leftOfBucket > 0 && this.buckets.has(leftOfBucket)) {
+        //            result.content = this.buckets.get(leftOfBucket);
+        //            break;
+        //        }
 
-                offset++;
-            }
+        //        offset++;
+        //    }
 
-        }
+        //}
+
+        var result = { returnType: constants.GET_CLOSEST_NODE_FOUND_NOTHING, content: {} };
+
+        var bucketIndex = this._selectBucketForReading(nodeId);
+
+        result = this._getTargetNodeElseGetBucketWithClosestNodes(nodeId, bucketIndex);
 
         return result;
 
