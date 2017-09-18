1dfc0f2da9a9c9f0d92299db581f8988bbc093e9
Brynleif Andreasen <brynleif@brynleif.com>
2017-09-18 17:54:46 +0200

    Bugfixes in bucketManager

--- HelloNode/bucketManager.js
+++ HelloNode/bucketManager.js
@@ -7,115 +7,120 @@ const bucket = require('./bucket');
 
 class BucketManager {
 
-   constructor() {
-      this.buckets = new Map();
-   }
+    constructor() {
+        this.buckets = new Map();
+    }
 
-   _selectBucketForInserting(contactId) {
-      var distance = utils.getDistance(globals.nodeId, contactId);
+    _selectBucketForInserting(contactId) {
+        var distance = utils.getDistance(globals.nodeId, contactId);
 
-      if (distance <= 0) {
-         return constants.ERROR_SELECT_BUCKET_DISTANCE_IS_ZERO;
-      }
+        if (distance <= 0) {
+            return constants.ERROR_SELECT_BUCKET_DISTANCE_IS_ZERO;
+        }
 
-      var x = 0;
-      while (true) {
-         if (Math.pow(2, x) <= distance && distance < Math.pow(2, x + 1)) {
-            break;
-         }
-         x++;
-      }
-
-      return x;
-   }
-
-   _selectBucketForReading(nodeIdOfCurrentNode) {
-      var distanceFromMeToTargetNode = utils.getDistance(nodeIdOfCurrentNode, globals.nodeId);
-      var bucketIndex = -1;
-
-      if (this.buckets.has(bucketIndex)) {
-         bucketIndex = Math.floor(Math.log2(distanceFromMeToTargetNode));
-      }
-      else {
-         var offset = 1;
-
-         while (offset <= constants.BUCKET_NEIGHBOUR_SEARCH_RANGE) {
-
-            var rightOfBucket = bucketIndex + offset;
-            var leftOfBucket = bucketIndex - offset;
-
-            if (this.buckets.has(rightOfBucket)) {
-               bucketIndex = rightOfBucket;
-               break;
+        var x = 0;
+        while (true) {
+            if (Math.pow(2, x) <= distance && distance < Math.pow(2, x + 1)) {
+                break;
             }
-            else if (leftOfBucket > 0 && this.buckets.has(leftOfBucket)) {
-               bucketIndex = leftOfBucket;
-               break;
+            x++;
+        }
+
+        return x;
+    }
+
+    _selectBucketForReading(nodeIdOfCurrentNode) {
+        var distanceFromMeToTargetNode = utils.getDistance(nodeIdOfCurrentNode, globals.nodeId);
+        var bucketIndex = Math.floor(Math.log2(distanceFromMeToTargetNode));
+
+        if (this.buckets.has(bucketIndex)) {
+            //do nothing
+        }
+        else {
+            var offset = 1;
+
+            while (offset <= constants.BUCKET_NEIGHBOUR_SEARCH_RANGE) {
+
+                var rightOfBucket = bucketIndex + offset;
+                var leftOfBucket = bucketIndex - offset;
+
+                if (this.buckets.has(rightOfBucket)) {
+                    bucketIndex = rightOfBucket;
+                    break;
+                }
+                else if (leftOfBucket > 0 && this.buckets.has(leftOfBucket)) {
+                    bucketIndex = leftOfBucket;
+                    break;
+                }
+
+                offset++;
             }
+        }
 
-            offset++;
-         }
-      }
+        return bucketIndex;
+    }
 
-      return bucketIndex;
-   }
+    _getTargetNodeElseGetBucketWithClosestNodes(nodeId, bucketIndex) {
 
-   _getTargetNodeElseGetBucketWithClosestNodes(nodeId, bucketIndex) {
-      var bucketWithClosestNodes = this.buckets.get(bucketIndex);
+        if (typeof this.buckets.get(bucketIndex) === 'undefined') {
+            return { returnType: constants.GET_CLOSEST_NODE_FOUND_NOTHING };
+        }
 
-      result = {};
+        var bucketWithClosestNodes = this.buckets.get(bucketIndex);
 
-      if (bucketWithClosestNodes.has(nodeId)) {
-         result.returnType = constants.GET_CLOSEST_NODE_FOUND_THE_NODE;
-         result.content = bucketWithClosestNodes.getFull(nodeId);
-      }
-      else {
-         result.returnType = constants.GET_CLOSEST_NODE_FOUND_A_BUCKET;
-         result.content = bucketWithClosestNodes.toJson();
-      }
+        var result = {};
 
-      return result;
-   }
+        if (bucketWithClosestNodes.has(nodeId)) {
+            result.returnType = constants.GET_CLOSEST_NODE_FOUND_THE_NODE;
+            result.content = bucketWithClosestNodes.getFull(nodeId);
+        }
+        else {
+            result.returnType = constants.GET_CLOSEST_NODE_FOUND_A_BUCKET;
+            result.content = bucketWithClosestNodes.toJson();
+        }
+
+        return result;
+    }
 
 
-   receiveNode(nodeId, contact) {
-      var appropriateBucket = this._selectBucketForInserting(nodeId);
+    receiveNode(nodeId, contact) {
+        var appropriateBucket = this._selectBucketForInserting(nodeId);
 
-      if (appropriateBucket === constants.ERROR_SELECT_BUCKET_DISTANCE_IS_ZERO) {
-         console.log("WARNING: Unable to add myself to a bucket.");
-         return false;
-      }
+        if (appropriateBucket === constants.ERROR_SELECT_BUCKET_DISTANCE_IS_ZERO) {
+            console.log("WARNING: Unable to add myself to a bucket.");
+            return false;
+        }
 
-      if (!this.buckets.has(appropriateBucket)) {
-         var tempBucket = new bucket();
-         this.buckets.set(appropriateBucket, tempBucket);
-      }
+        if (!this.buckets.has(appropriateBucket)) {
+            var tempBucket = new bucket();
+            this.buckets.set(appropriateBucket, tempBucket);
+        }
 
-      this.buckets.get(appropriateBucket).set(nodeId, contact);
-   }
+        this.buckets.get(appropriateBucket).set(nodeId, contact);
+    }
 
-   getClosestNodes(nodeId) {
-      var result = { returnType: constants.GET_CLOSEST_NODE_FOUND_NOTHING, content: {} };
+    getClosestNodes(nodeId) {
+        var result = { returnType: constants.GET_CLOSEST_NODE_FOUND_NOTHING, content: {} };
 
-      var bucketIndex = this._selectBucketForReading(nodeId);
+        var bucketIndex = this._selectBucketForReading(nodeId);
 
-      result = this._getTargetNodeElseGetBucketWithClosestNodes(nodeId, bucketIndex);
+        result = this._getTargetNodeElseGetBucketWithClosestNodes(nodeId, bucketIndex);
 
-      return result;
-   }
+        return result;
+    }
 
-   getBuckets() {
-      var result = [];
-      var self = this;
-      //iterate through the buckets
-      this.buckets.forEach(function (value, key, map) {
-         var bucketJson = value.toJson();
+    getBuckets() {
+        var result = [];
+        var self = this;
+        //iterate through the buckets
+        this.buckets.forEach(function (value, key, map) {
+            var bucketJson = value.toJson();
 
-         result.push({ key: key, nodes: bucketJson });
-      })
+            result.push({ key: key, nodes: bucketJson });
+        })
 
-      return result;
-   }
+        return result;
+    }
 }
 
 module.exports = BucketManager;
