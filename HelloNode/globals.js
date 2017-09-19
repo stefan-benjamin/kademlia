f3441cb4226e76f193004584749f5dff8f8c69cd
Stefan Bejan <stefan.bejan90@gmail.com>
2017-09-19 10:35:30 +0200

    Small startup port configuration change

--- HelloNode/globals.js
+++ HelloNode/globals.js
@@ -1,7 +1,7 @@
 ﻿module.exports = {
 
    //Default value, used when debugging in VS.
-   portNumber : 8082,
+   portNumber : 8080,
    initialNodeIpAddress: '127.0.0.1',
    initialNodePortNumber: 8080,
    nodeId: null,
