25647167fc0599b9140d2b9b069bfa23445b2850
daniop301 <danielpytlos@gmail.com>
2017-09-26 15:39:10 +0200

    Added store GUI options

--- HelloNode/public/js/view.js
+++ HelloNode/public/js/view.js
@@ -11,9 +11,19 @@
          $("#resultDiv").empty().append(JSON.stringify(resultData.result));
 
       });
+    });
 
-      
+   $("#storeButton").click(function () {
+       console.log("Store button clicked");
 
+       var key = $("#keyInput").val();
+
+       var value = $("#valueInput").val();
+
+       $.post('/api/store', key, value, function (resp) {
+           $("#storeResultDiv").empty().append(resp)
+       });
    });
+
 });
 
